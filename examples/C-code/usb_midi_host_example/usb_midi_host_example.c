c98ccdc7f1983987a2ff8a22ac9f53fc4315ee08
Robert Manzke <robert.manzke@fh-kiel.de>
2024-06-14 11:37:08 +0200

    testing

--- examples/C-code/usb_midi_host_example/usb_midi_host_example.c
+++ examples/C-code/usb_midi_host_example/usb_midi_host_example.c
@@ -261,6 +261,7 @@ void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance)
 
 void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets)
 {
+    printf("Midi Callback\r\n");
   if (midi_dev_addr == dev_addr) {
     if (num_packets != 0) {
       uint8_t cable_num;
